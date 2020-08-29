9928e2c71ca2e82ec58f0626978e4ffd160ea497
xiechaokang <x1234567>
2020-08-29 15:34:16 +0800

    first

---
+++ pay/src/main/java/com/xck/main/PayMainApplication.java
@@ -0,0 +1,15 @@
+package com.xck.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xck
+ */
+
+@SpringBootApplication
+public class PayMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PayMainApplication.class,args);
+    }
+}
