2a22a41356909f467042ab413500062740838c83
xiechaokang <x1234567>
2020-09-04 18:51:37 +0800

    #

--- pay/src/main/java/com/xck/main/PayMainApplication.java
+++ pay/src/main/java/com/xck/main/PayMainApplication.java
@@ -1,5 +1,6 @@
 package com.xck.main;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@MapperScan("com.xck.main.mapper")
 public class PayMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(PayMainApplication.class,args);
